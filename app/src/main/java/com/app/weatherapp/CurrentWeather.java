e9d128aaaa3d736c34a3941c26eeecea84b03bef
dolly goyal <d.017goyal@gmail.com>
2015-02-28 21:40:22 +0530

    Json data not work

--- app/src/main/java/com/app/weatherapp/CurrentWeather.java
+++ app/src/main/java/com/app/weatherapp/CurrentWeather.java
@@ -16,23 +16,28 @@ public class CurrentWeather {
     private String mTimeZone;
 
     public String getTimeZone() {
+
         return mTimeZone;
     }
 
     public void setTimeZone(String timeZone) {
+
         mTimeZone = timeZone;
     }
 
     public String getSummary() {
+
         return mSummary;
     }
 
     public void setSummary(String summary) {
+
         mSummary = summary;
     }
 
-    public double getPrecipChance() {
-        return mPrecipChance;
+    public int getPrecipChance() {
+        double precipPercentage = mPrecipChance * 100;
+        return (int)Math.round(precipPercentage);
     }
 
     public void setPrecipChance(double precipChance) {
@@ -40,19 +45,58 @@ public class CurrentWeather {
     }
 
     public double getHumidity() {
+
         return mHumidity;
     }
 
     public void setHumidity(double humidity) {
+
         mHumidity = humidity;
     }
 
-    public double getTemperature() {
-        return mTemperature;
+    public int getTemperature() {
+        return (int)Math.round(mTemperature);
     }
 
     public void setTemperature(double temperature) {
         mTemperature = temperature;
+    }
+
+    public int getIconId(){
+        int iconId = R.drawable.clear_day;
+
+        if (mIcon.equals("clear-day")){
+            iconId = R.drawable.clear_day;
+        }
+        else if (mIcon.equals("clear-night")){
+            iconId = R.drawable.clear_night;
+        }
+        else if (mIcon.equals("rain")){
+            iconId = R.drawable.rain;
+        }
+        else if (mIcon.equals("snow")){
+            iconId = R.drawable.snow;
+        }
+        else if (mIcon.equals("sleet")){
+            iconId = R.drawable.sleet;
+        }
+        else if (mIcon.equals("wind")){
+            iconId = R.drawable.wind;
+        }
+        else if (mIcon.equals("fog")){
+            iconId = R.drawable.fog;
+        }
+        else if (mIcon.equals("cloudy")){
+            iconId = R.drawable.cloudy;
+        }
+        else if (mIcon.equals("cloudy-night")){
+            iconId = R.drawable.cloudy_night;
+        }
+        else if (mIcon.equals("partially-cloudy-night")){
+            iconId = R.drawable.partly_cloudy;
+        }
+
+        return iconId;
     }
 
     public long getTime() {
@@ -69,10 +113,12 @@ public class CurrentWeather {
     }
 
     public void setTime(long time) {
+
         mTime = time;
     }
 
     public String getIcon() {
+
         return mIcon;
     }
 
--- app/src/main/java/com/app/weatherapp/MainActivity.java
+++ app/src/main/java/com/app/weatherapp/MainActivity.java
@@ -5,11 +5,16 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.nfc.Tag;
-import android.support.v7.app.ActionBarActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.okhttp.Call;
@@ -23,23 +28,52 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 
 public class MainActivity extends ActionBarActivity {
     public static final String TAG = MainActivity.class.getSimpleName();
     private CurrentWeather mCurrentWeather;
 
+    @InjectView(R.id.timeLabel) TextView mTimeLabel;
+    @InjectView(R.id.temperatureLabel) TextView mTemperatureLabel;
+    @InjectView(R.id.humidityLabel) TextView mHumidityValue;
+    @InjectView(R.id.precipLabel) TextView mPrecipValue;
+    @InjectView(R.id.summaryLabel) TextView mSummaryLabel;
+    @InjectView(R.id.iconImageView) ImageView mIconImageView;
+    @InjectView(R.id.refreshImagiew) ImageView mRefreshImageView;
+    @InjectView(R.id.progressBar)ProgressBar mProgressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+        mProgressBar.setVisibility(View.INVISIBLE);
 
+        final double latitude = 12.876804;
+        final double longitude = 77.724194;
+
+        mRefreshImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getForecast(latitude, longitude);
+            }
+        });
+        getForecast(latitude, longitude);
+
+        Log.d(TAG, "Main UI code is running");
+    }
+
+    private void getForecast(double latitude, double longitude) {
         String apiKey = "3d2f3ea7ddbe6576177030b6b9662267";
-        double latitude = 37.8267;
-        double longitude = -122.423;
-        String forecastUrl = "https://api.forecast.io/forecast/"
+                String forecastUrl = "https://api.forecast.io/forecast/"
                 + apiKey + "/"
                 + latitude + "," + longitude;
         if (isNetworkAvailable()) {
+            toggelRefresh();
+
             OkHttpClient client = new OkHttpClient();
             Request request = new Request.Builder()
                     .url(forecastUrl)
@@ -48,21 +82,40 @@ public class MainActivity extends ActionBarActivity {
             call.enqueue(new Callback() {
                 @Override
                 public void onFailure(Request request, IOException e) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            toggelRefresh();
 
+                        }
+                    });
+                    alertUserAbouError();
                 }
 
                 @Override
                 public void onResponse(Response response) throws IOException {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            toggelRefresh();
+
+                        }
+                    });
                     try {
-                        String jsonData = response.body().string();
+                        String jsonData = response.body().toString();
                         Log.v(TAG, jsonData);
                         if (response.isSuccessful()) {
+
                             mCurrentWeather = getCurrentDetails(jsonData);
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    updateDisplay();
+                                }
+                            });
                         } else {
                             alertUserAbouError();
                         }
-                    } catch (IOException e) {
-                        Log.e(TAG, "Exception caught: ", e);
                     } catch (JSONException e){
                         Log.e(TAG, "Exception caught: ", e);
                     }
@@ -73,7 +126,25 @@ public class MainActivity extends ActionBarActivity {
             Toast.makeText(this, getString(R.string.network_unavailable_message),
                     Toast.LENGTH_LONG).show();
         }
-        Log.d(TAG, "Main UI code is running");
+    }
+
+    private void toggelRefresh() {
+        if (mProgressBar.getVisibility() == View.INVISIBLE) {
+            mProgressBar.setVisibility(View.VISIBLE);
+            mRefreshImageView.setVisibility(View.INVISIBLE);
+        }
+        else{
+            mProgressBar.setVisibility(View.INVISIBLE);
+            mRefreshImageView.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void updateDisplay() {
+        mTemperatureLabel.setText(mCurrentWeather.getTemperature() + "");
+        mTimeLabel.setText("At" + mCurrentWeather.getFormattedTime() + "it will be");
+        mHumidityValue.setText(mCurrentWeather.getHumidity() + "");
+        mPrecipValue.setText(mCurrentWeather.getPrecipChance() + "%");
+        mSummaryLabel.setText(mCurrentWeather.getSummary() + "");
     }
 
     private CurrentWeather getCurrentDetails(String jsonData) throws JSONException{
@@ -86,9 +157,9 @@ public class MainActivity extends ActionBarActivity {
         CurrentWeather currentWeather = new CurrentWeather();
         currentWeather.setHumidity(currently.getDouble("humidity"));
         currentWeather.setTime(currently.getLong("time"));
-        currentWeather.setIcon(currently.getString("Icon"));
-        currentWeather.setPrecipChance(currently.getDouble("PrecipChance"));
-        currentWeather.setSummary(currently.getString("Summary"));
+        currentWeather.setIcon(currently.getString("icon"));
+        //currentWeather.setPrecipChance(currently.getDouble("precipChance"));
+        currentWeather.setSummary(currently.getString("summary"));
         currentWeather.setTemperature(currently.getDouble("temperature"));
         currentWeather.setTimeZone(timezone);
 
